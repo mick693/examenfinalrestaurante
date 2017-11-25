393d39cc385ce51dbda13586dca8bc5487e12fc6
mick693 <jmiguelcl47@gmail.com>
2017-11-24 19:52:44 -0600

    segundo commit

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Plato
 
+def plato_list(request):
+    plato =Plato.objects.all()
+    return render(request,'blog/plato_list.html',{'plato':plato})
 # Create your views here.
